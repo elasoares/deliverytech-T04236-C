4323f6682e5f8119610cadf0e9ca7fc4f85c6025
Elaine <elainehap@gmail.com>
2026-06-03 22:25:03 -0300

    Acrescentado informações no arquivo dataloader

--- src/main/java/com/deliverytech/delivery/config/DataLoader.java
+++ src/main/java/com/deliverytech/delivery/config/DataLoader.java
@@ -1,19 +1,27 @@
 package com.deliverytech.delivery.config;
 
 import com.deliverytech.delivery.model.Cliente;
+import com.deliverytech.delivery.model.Produto;
+import com.deliverytech.delivery.model.Restaurante;
 import com.deliverytech.delivery.repository.ClienteRepository;
+import com.deliverytech.delivery.repository.ProdutoRepository;
+import com.deliverytech.delivery.repository.RestauranteRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
 
 @Configuration
 public class DataLoader {
 
     @Bean
-    public CommandLineRunner initData(ClienteRepository clienteRepository){
+    public CommandLineRunner initData(ClienteRepository clienteRepository, RestauranteRepository restauranteRepository, ProdutoRepository produtoRepository){
 
         return args ->{
             System.out.println("Iniciando carga de dados...");
+
+            System.out.println("Cliente criado: ");
 
             Cliente c1 = new Cliente();
             c1.setNome("Wilson Martins");
@@ -24,13 +32,41 @@ public class DataLoader {
 
             clienteRepository.save(c1);
 
-            System.out.println("Usuário " + c1.getNome() + "do e-mail " + c1.getEmail() + "cadastrado com sucesso!");
+            System.out.println("Usuário cliente " + c1.getNome() + " do e-mail " + c1.getEmail() + " cadastrado com sucesso!");
 
-            System.out.println("Nome:");
-            System.out.println(c1.getNome());
+            System.out.println("Restaurante criado: ");
 
-            System.out.print("Data cadastro:");
-            System.out.print(c1.getDataCadastro());
+            Restaurante r1 = new Restaurante();
+            r1.setNome("Pizza Top");
+            r1.setAtivo(true);
+            r1.setEndereco("Rua 2, B");
+            r1.setTelefone("1192222-2222");
+            r1.setCategoria("Pizzaria");
+            r1.setAvaliacao(new BigDecimal(4.5));
+
+            restauranteRepository.save(r1);
+
+            System.out.println("Usuário restaurante " + r1.getNome() + " cadastrado com sucesso!" );
+            System.out.println(r1.getCategoria());
+            System.out.println(r1.getProdutos());
+            System.out.println(r1.getTelefone());
+
+            System.out.println("Produto criado: ");
+
+            Produto p1 = new Produto();
+            p1.setNome("Pizza de Calabresa");
+            p1.setCategoria("Pizza");
+            p1.setDescricao("Pizza de calabresa com queijo");
+            p1.setPreco(new BigDecimal("45.00"));
+            p1.setRestaurante(r1);
+
+            /*produtoRepository.save(p1);*/
+
+            System.out.println("Produto " + p1.getNome() + " Foi cadastrado com sucesso!");
+            System.out.print("Preço: ");
+            System.out.println(p1.getPreco());
+
+
         };
     }
 
